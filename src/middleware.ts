3dc8b093eb7dfc288898a32342a839ed9e46ae58
Joe Cusic <your-github-email@example.com>
2025-09-29 00:35:04 -0500

    Fix: removed /home redirect from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,5 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-  if (pathname === "/home") {
-    return NextResponse.redirect(new URL("/", req.url), 308);
-  }
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ["/home"],
-};
