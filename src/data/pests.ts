5e66fada7fc661ffae6de7a14c0eb110987f693f
Joe Cusic <your-github-email@example.com>
2025-09-28 20:47:49 -0500

    Cleanup: removed circular Pest import

--- src/data/pests.ts
+++ src/data/pests.ts
@@ -2,3 +2,4 @@ export type { Pest } from './pests.generated';
 export { pests, getPest } from './pests.generated';
 export type { PestDetails } from './descriptions';
 export { DESCRIPTIONS } from './descriptions';
+
